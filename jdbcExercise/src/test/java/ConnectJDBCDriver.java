d1b7d2d5c10fa2068db26066d3e8e7cc053aadba
927138 <jiyh1541@gmail.com>
2023-12-01 11:08:37 +0900

    jdbc basic

---
+++ jdbcExercise/src/test/java/ConnectJDBCDriver.java
@@ -0,0 +1,58 @@
+import com.yh.jdbcexercise.ConnectionMysql;
+import java.sql.Connection;
+import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+@Slf4j
+public class ConnectJDBCDriver {
+
+     private static Connection connection = null;
+
+     @BeforeAll
+     static void setUp(){
+          log.info("connection open()");
+          connection = ConnectionMysql.getConnection();
+     }
+
+     @AfterAll
+     static void release(){
+          log.info("connection close()");
+          try {
+               connection.close();
+          } catch (SQLException e){
+               throw new RuntimeException(e);
+          }
+     }
+
+     @Test
+     @DisplayName("connected")
+     void connectTest(){
+          Assertions.assertNotNull(connection);
+     }
+
+     @Test
+     @DisplayName("mariadb driver load : fail")
+     void mariaDB_driverLoadTest(){
+          Assertions.assertThrows(ClassNotFoundException.class,
+                  ()->Class.forName("org.mariadb.jdbc.Driver"));
+     }
+
+     @Test
+     @DisplayName("mysql driver load : success")
+     void mysql_dirverLoadTest(){
+          try{
+               Class<?> driver = Class.forName("com.mysql.cj.jdbc.Driver");
+               log.info("driver : " + driver.getName());
+
+               Assertions.assertEquals(driver.getName(), com.mysql.cj.jdbc.Driver.class.getName());
+          }catch (ClassNotFoundException e){
+               log.error("mysql driver load : fail");
+          }
+     }
+}
---
+++ jdbcExercise/src/test/java/JdbcExceptionTset.java
@@ -0,0 +1,37 @@
+import com.yh.jdbcexercise.ConnectionMysql;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class JdbcExceptionTset {
+
+     @Test
+     @DisplayName("query error")
+     void test(){
+
+          String query = "insert into example_student values(100, 'test', 'M' 20)";
+
+          try (Connection connection = ConnectionMysql.getConnection()){
+               SQLException exception = Assertions.assertThrows(SQLException.class,
+                    ()-> {
+                         Statement statement = connection.createStatement();
+                         statement.execute(query);
+                    });
+
+               log.info("errorCode : {}", exception.getErrorCode());
+               log.info("message : {}", exception.getMessage());
+               log.info("sqlState : ", exception.getSQLState());
+          } catch (SQLException e){
+               e.getSQLState();
+
+          }
+
+     }
+}
