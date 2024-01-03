f89d98168c7cf72869967e2e8a93a8153d981dac
927138 <jiyh1541@gmail.com>
2024-01-03 23:45:19 +0900

    test commit

--- Spring core/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/pattern/factory/CsvDocument.java
+++ Spring core/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/pattern/factory/CsvDocument.java
@@ -3,7 +3,7 @@ package com.nhnacademy.edu.springframework.pattern.factory;
 public class CsvDocument extends Document{
 
      @Override
-     public void open(){
+     public void open(){t
           System.out.println("open the csvDocument");
      }
 }
