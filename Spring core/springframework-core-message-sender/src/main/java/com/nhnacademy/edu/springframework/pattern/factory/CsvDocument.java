e9ef30afed4ecec2991f5c7d3f7d1789821be19c
927138 <jiyh1541@gmail.com>
2024-01-05 09:15:13 +0900

    update

--- Spring core/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/pattern/factory/CsvDocument.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.edu.springframework.pattern.factory;
-
-public class CsvDocument extends Document{
-
-     @Override
-     public void open(){t
-          System.out.println("open the csvDocument");
-     }
-}
--- Spring core/springframework-core-message-sender/src/main/java/com/nhnacademy/edu/springframework/pattern/factory/Document.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.edu.springframework.pattern.factory;
-
-public abstract class Document {
-     public abstract void open();
-}
