34554c214404eb603df36dcf4e78e18adc5aaaf3
927138 <jiyh1541@gmail.com>
2024-01-16 22:25:49 +0900

    add : shop.sql schema constraint

--- Spring-JPA/academy-spring-jpa/src/main/resources/script/shop.sql
+++ Spring-JPA/academy-spring-jpa/src/main/resources/script/shop.sql
@@ -1,52 +1,72 @@
 CREATE TABLE if not exists Categories (
-                            category_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                            category_name VARCHAR(50)
+                            category_id BIGINT AUTO_INCREMENT,
+                            category_name VARCHAR(50),
+
+                            PRIMARY KEY (category_id)
 );
 
 CREATE TABLE if not exists Products (
-                          prod_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          prod_id BIGINT AUTO_INCREMENT,
                           category_id BIGINT,
                           model_number NVARCHAR(10),
                           model_name NVARCHAR(120),
                           prod_image NVARCHAR(30),
                           unit_cost DECIMAL(15),
-                          description text
+                          description text,
 
+                          PRIMARY KEY (prod_id),
+                          FOREIGN KEY (category_id) REFERENCES Categories(category_id)
 );
 
 CREATE TABLE if not exists Customers (
-                           cus_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                           cus_id BIGINT AUTO_INCREMENT,
                            name VARCHAR(10),
                            email_address VARCHAR(100) UNIQUE,
-                           password VARCHAR(12)
+                           password VARCHAR(12),
+
+                           PRIMARY KEY (cus_id)
 );
 
 CREATE TABLE if not exists Reviews (
-                         review_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         review_id BIGINT AUTO_INCREMENT,
                          prod_id BIGINT,
                          cus_id BIGINT,
                          rating INT,
-                         comments TEXT
+                         comments TEXT,
+
+                         PRIMARY KEY (review_id),
+                         FOREIGN KEY (prod_id) REFERENCES Products(prod_id),
+                         FOREIGN KEY (cus_id) REFERENCES Customers(cus_id)
 );
 
 CREATE TABLE if not exists Orders (
-                        order_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        order_id BIGINT AUTO_INCREMENT,
                         cus_id BIGINT,
                         order_date TIMESTAMP,
-                        ship_date TIMESTAMP
+                        ship_date TIMESTAMP,
+
+                        PRIMARY KEY (order_id),
+                        FOREIGN KEY (cus_id) REFERENCES Customers(cus_id)
 );
 
 CREATE TABLE if not exists OrderDetails (
                               order_id BIGINT,
                               prod_id BIGINT,
                               quantity INT,
-                              unit_cost DECIMAL(15)
+                              unit_cost DECIMAL(15),
+
+                              PRIMARY KEY (order_id, prod_id),
+                              FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+                              FOREIGN KEY (prod_id) REFERENCES Products(prod_id)
 );
 
 CREATE TABLE ShoppingCart (
-                              record_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                              record_id BIGINT AUTO_INCREMENT,
                               cart_id NVARCHAR(150),
                               quantity INT,
                               prod_id BIGINT,
-                              date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                              date_created TIMESTAMP,
+
+                              PRIMARY KEY (record_id),
+                              FOREIGN KEY (prod_id) REFERENCES Products(prod_id)
 );
