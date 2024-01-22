fdec7ee02782e6e0a28926dbdd53afa9eb064fbe
927138 <jiyh1541@gmail.com>
2024-01-22 20:14:49 +0900

    remove : student project

--- Spring-Boot/student/src/main/java/com/nhnacademy/edu/springboot/student/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.nhnacademy.edu.springboot.student.repository;
-//
-//import com.nhnacademy.edu.springboot.student.domain.Student;
-//import java.util.List;
-//
-//public interface StudentRepository {
-//     List<Student> findAll();
-//}
